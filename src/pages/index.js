7a0800a10d8afffb59f79b6e9e18ef7b9852e673
wlcharlie <wl00606352@gmail.com>
2022-04-02 19:56:37 +0800

    button to github

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import LinearSearchPage from "pages/linear-search"
 import BinarySearchPage from "pages/binary-search"
 import Header from "components/Header"
 
-import { Box } from "@chakra-ui/react"
+import { Box, Tag, Tooltip } from "@chakra-ui/react"
 
 export default function Router() {
   return (
-    <Box width="100vw" height="100vh">
+    <Box width="100vw" height="100vh" position="relative">
       <Header />
       <Box width="100%" height="93vh">
         <Routes>
@@ -15,6 +15,20 @@ export default function Router() {
           <Route path="/binary-search" element={<BinarySearchPage />} />
         </Routes>
       </Box>
+      <Tooltip label="Submit a issue for me!">
+        <Tag
+          position="fixed"
+          left={5}
+          bottom={5}
+          size="md"
+          as="a"
+          href="https://github.com/wlcharlie/algo-visual"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Found Bug?
+        </Tag>
+      </Tooltip>
     </Box>
   )
 }
